e8bada583b2b3d142595815852cef28a1750c2cd
James Kropp <xflat@hotmail.ca>
2017-03-16 15:09:26 -0700

    Rebuilt to allow user input
    Allow people to enter their own Github URL's to view the project
    filestructure. Also removed Jquery and used a basic Ajax API.

--- js/my.js
+++ js/my.js
@@ -1,94 +1,114 @@
-$(document).ready(function() {
-    view();
-    window.paths = [];
+window.paths = [];
 
-    String.prototype.count = function(s1) { // Counting function from Stackoverflow - http://stackoverflow.com/questions/881085/count-the-number-of-occurences-of-a-character-in-a-string-in-javascript -- The answer from: Immibis
-        return (this.length - this.replace(new RegExp(s1, "g"), '').length) / s1.length;
-    }
+String.prototype.count = function(s1) { // Counting function from Stackoverflow - http://stackoverflow.com/questions/881085/count-the-number-of-occurences-of-a-character-in-a-string-in-javascript -- The answer from: Immibis
+    return (this.length - this.replace(new RegExp(s1, "g"), '').length) / s1.length;
+}
 
-    function view() {
-        $.ajax({
-            url: "https://api.github.com/repos/jameskropp/shoption/git/trees/master?recursive=1", // Github project URL
-            type: "get",
-            data: {
-                type: "view"
-            },
-            dataType: "json",
-            success: function(resp) {
-                for (var i = 0; i < resp.tree.length; i++) {
-                    var str = resp.tree[i].path;
+var Controls = React.createClass({
 
-                    if (str.indexOf("/") > -1) {
-                        var split = str.substring(str.lastIndexOf("/") + 1); // Find the last "/" in the string
-                        var slashes = str.count("/"); // Use the prototype count function at the top of the file
-                        var addDash = " ";
-                        for (var b = 0; b < slashes; b++) {
-                            addDash += "- "; // Adding dashes if its in a file structure
-                        }
-                        paths.push(addDash + split);
-                    } else if (str.indexOf('/') == -1) {
-                        if (str.indexOf(".") == -1) {
-                            paths.push("Directory: " + str); // If there is no "/" or "." it will be a directory
-                        } else {
-                            paths.push(str); // If there is a dot it will be a file.
-                        }
-                    }
-                }
-                loadApp(); // Now that everything is loaded run my React to show everything.
-            }
+    addUrl: function() {
+        paths.length = 0;
+        this.searchGit(document.getElementById('url').value);
+        document.getElementById('url').value = '';
+    },
+
+    searchGit: function(url) {
+      var self = this;
+      axios.get("https://api.github.com/repos/" + url + "/git/trees/master?recursive=1")
+        .then(function(result){
+          self.props.sortPaths(result);
         })
+    },
+
+    render: function() {
+        return (
+            <div className="controls">
+            <h3>Project - Directory</h3>
+            <p>Grab the name of the user and the project link.<br />
+            Example: "https://github.com/jameskropp/Shoption" you would enter "jameskropp/shoption"</p>
+            <input id="url" placeholder="jameskropp/shoption" />
+            <button onClick={this.addUrl}>Search</button>
+            <hr />
+            <p> A "-" means one directory in.</p>
+          </div>
+        )
     }
 })
 
-function loadApp() {
-
-    var CreatePath = React.createClass({
-        render: function() {
-            var paths = this.props.paths.map(function(obj, index) {
-                return (
-                  <div key={index}><p>{obj}</p></div> // Grab the array of paths and print them out
-                )
-            })
+var ViewPath = React.createClass({
+    render: function() {
+        var paths = this.props.paths.map(function(obj, index) {
             return (
-              <div className="paths">
-                <h3>Shoption - Directory</h3>
-                <p> A "-" means one directory in.</p>
-                <hr />
-                {paths}
-              </div> // Call the print function
+                <div key={index}><p>{obj}</p></div> // Grab the array of paths and print them out
             )
-        }
-    })
-
-    var CreateSide = React.createClass({
-      render: function(){
+        })
         return (
-          <div className="sidebar">
+            <div className="paths">
+                {paths}
+            </div>
+        )
+    }
+})
+
+var CreateSide = React.createClass({
+    render: function() {
+        return (
+            <div className="sidebar">
             <h3>James Kropp</h3>
             <h3>Github Project File Structure</h3>
           </div>
         )
-      }
-    })
+    }
+})
 
-    var App = React.createClass({
-        getInitialState: function() {
-            return {
-                paths: window.paths // Grab the path array
-            }
-        },
-        render: function() {
-            return (
-              <div className="content">
-                <CreateSide />
-                <CreatePath paths = {this.state.paths} />
-              </div>
-            )
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            paths: window.paths, // Grab the path array
         }
-    })
+    },
+    sortPaths: function(result) {
+      var resp = result.data;
+      for (var i = 0; i < resp.tree.length; i++) {
+          var str = resp.tree[i].path;
 
-    ReactDOM.render(
-      <App /> ,
-        document.getElementById('display')
-    );
-}
+          if (str.indexOf("/") > -1) {
+              var split = str.substring(str.lastIndexOf("/") + 1); // Find the last "/" in the string
+              var slashes = str.count("/"); // Use the prototype count function at the top of the file
+              var addDash = " ";
+              for (var b = 0; b < slashes; b++) {
+                  addDash += "- "; // Adding dashes if its in a file structure
+              }
+              if (resp.tree[i].type == "tree") {
+                  this.state.paths.push(addDash + "Directory: " + str); // If the type is a tree that means its a directory
+              } else {
+                  this.state.paths.push(addDash + split);
+              }
+          } else {
+              if (resp.tree[i].type == "tree") {
+                  this.state.paths.push("Directory: " + str); // If the type is a tree that means its a directory
+              } else {
+                  this.state.paths.push(str); // Anything not in a directory
+              }
+          }
+      }
+      this.setState({
+        paths: window.paths
+      })
+      console.log(window.paths)
+    },
+    render: function() {
+        return (
+            <div className="content">
+              <CreateSide />
+              <Controls sortPaths={this.sortPaths} newPaths={this.newPaths} paths={this.state.paths} />
+              <ViewPath paths={this.state.paths} />
+            </div>
+        )
+    }
+})
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('display')
+);
